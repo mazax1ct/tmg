1e46d9a72628d0f50238f78aecc6001056f34353
mazax1ct <vova@giveandget.ru>
2021-11-04 17:26:19 +0300

    start

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  //кастомный селект
+	/*$('.js-select').each(function() {
+   var $p = $(this).closest('.select-wrapper__container');
+   $(this).select2({
+		 minimumResultsForSearch: Infinity,
+     dropdownPosition: 'below',
+     dropdownParent: $p,
+     placeholder: $(this).attr('placeholder')
+   });
+	}).on("select2:open", function (e) {
+		var $p = $(this).closest('.select-wrapper__container');
+		$p.addClass('open');
+	}).on("select2:close", function (e) {
+		var $p = $(this).closest('.select-wrapper__container');
+		$p.removeClass('open');
+	});*/
+});
