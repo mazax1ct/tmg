7f15ead005937cde66c6930369fc23744dea3752
mazax1ct <vova@giveandget.ru>
2021-11-05 23:26:23 +0300

    search+mini-cart

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,24 @@
+//залипание шапки
+var scrolled;
+
+$(document).ready(function() {
+	scrolled = $(window).scrollTop();
+	if (scrolled > $('.header').height()*0.5) {
+		$('.header').addClass('scrolled');
+	} else {
+		$('.header').removeClass('scrolled');
+	}
+});
+
+$(window).scroll(function() {
+	scrolled = $(window).scrollTop();
+	if (scrolled > $('.header').height()*0.5) {
+		$('.header').addClass('scrolled');
+	} else {
+		$('.header').removeClass('scrolled');
+	}
+});
+
 //тоглер меню каталога
 $(document).on('click', '.js-catalog-toggler', function () {
   var _this = $(this);
@@ -17,7 +38,7 @@ $(document).on('click', '.js-catalog-toggler', function () {
 //тоглер категорий поиска
 $(document).on('click', '.js-search-categories', function () {
   var _this = $(this);
-  var dropdown = $('.search-bar__categories-block .dropdown');
+  var dropdown = $('.search-bar__categories-dropdown');
 
   if(_this.hasClass('is-active')) {
     dropdown.removeClass('is-open');
@@ -37,10 +58,10 @@ $(document).on('click', '.js-search-categories', function () {
 });
 
 //выбор категории
-$(document).on('click', '.search-bar__categories-block .dropdown__list-item', function () {
+$(document).on('click', '.categories-list__item', function () {
   var _this = $(this);
   var _text = _this.text();
-  $('.search-bar__categories-block .dropdown__list-item').removeClass('selected');
+  $('.categories-list__item').removeClass('selected');
   _this.addClass('selected');
 
   $('.search-bar__categories-toggler span').text(_text);
@@ -51,20 +72,61 @@ $(document).on('click', '.search-bar__categories-block .dropdown__list-item', fu
 
 //ввод текста в поле поиска
 $(document).on('input', '.search-bar__input', function () {
-	if($(this).val().length > 0) {
+	var _this = $(this);
+  var dropdown = $('.search-bar__dropdown');
+
+	if(_this.val().length > 0) {
 		$('.search-bar__input-clear').prop('disabled', '');
 		$('.search-bar__submit').prop('disabled', '');
-	}else{
+	} else {
 		$('.search-bar__input-clear').prop('disabled', 'disabled');
 		$('.search-bar__submit').prop('disabled', 'disabled');
+	}
+
+	if(_this.val().length > 2) {
+		dropdown.addClass('is-active');
+    setTimeout(function() {
+      dropdown.addClass('is-open');
+    }, 300);
+	} else {
+		dropdown.removeClass('is-open');
+    setTimeout(function() {
+      dropdown.removeClass('is-active');
+    }, 300);
 	}
 });
 
 //очистка поля ввода
 $(document).on('click', '.search-bar__input-clear', function () {
+	var dropdown = $('.search-bar__dropdown');
+	dropdown.removeClass('is-open');
+	setTimeout(function() {
+		dropdown.removeClass('is-active');
+	}, 300);
 	$('.search-bar__input').val('');
 	$('.search-bar__input-clear').prop('disabled', 'disabled');
 	$('.search-bar__submit').prop('disabled', 'disabled');
 	$('.search-bar__input').focus();
+	return false;
+});
+
+//тоглер корзины
+$(document).on('click', '.js-cart-toggler', function () {
+	var _this = $(this);
+  var dropdown = $('.cart-block__dropdown');
+
+  if(_this.hasClass('is-active')) {
+    dropdown.removeClass('is-open');
+    setTimeout(function() {
+      dropdown.removeClass('is-active');
+    }, 300);
+    _this.removeClass('is-active');
+  } else {
+    dropdown.addClass('is-active');
+    setTimeout(function() {
+      dropdown.addClass('is-open');
+    }, 100);
+    _this.addClass('is-active');
+  }
 	return false;
 });
