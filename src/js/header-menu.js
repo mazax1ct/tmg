2b6b9971c3dbfdd0a005fbf128a31153db54bdc1
mazax1ct <vova@giveandget.ru>
2021-11-07 22:05:18 +0300

    small fix for menu

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -1,10 +1,5 @@
 $(document).on('click', '.js-menu-opener', function () {
-  if(!$('body').hasClass('compensate-for-scrollbar')) {
-    scrollCompensate();
-  }
-
   $('body').addClass('overflow');
-
   $('.header-menu__outer').fadeIn('300', function () {
     $('.header-menu__inner').addClass('is-open');
     document.addEventListener('click', closeMenu);
@@ -17,10 +12,6 @@ $(document).on('click', '.js-menu-closer', function () {
   setTimeout(function() {
     $('.header-menu__outer').fadeOut();
     $('body').removeClass('overflow');
-
-    $('body').removeClass('compensate-for-scrollbar');
-    $('#scroll-compensate').remove();
-
     document.removeEventListener('click', closeMenu);
   }, 300);
   return false;
@@ -29,14 +20,9 @@ $(document).on('click', '.js-menu-closer', function () {
 function closeMenu(evt) {
   if (!$('.header-menu__inner').is(evt.target) && $('.header-menu__inner').has(evt.target).length === 0) {
     $('.header-menu__inner').removeClass('is-open');
-
     setTimeout(function() {
       $('.header-menu__outer').fadeOut();
       $('body').removeClass('overflow');
-
-      $('body').removeClass('compensate-for-scrollbar');
-			$('#scroll-compensate').remove();
-
       document.removeEventListener('click', closeMenu);
     }, 300);
 	}
